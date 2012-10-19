a00baa2ea6a76d1e59e88b69cea23f9ae2b60a55
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2012-10-19 18:10:26 +0200

    maybe fixed distance error

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <cassert>
+#include <deque>
 #include <algorithm>
 #include <unordered_set>
 #include "config.hpp"
@@ -14,56 +15,83 @@ using namespace std;
 using namespace tree;
 
 
+void test ( uint runs ) {
 
-size_t distance ( const ztree<N>& a, const ztree<N>& b, size_t& visited,
-                  float& load, size_t& buckets ) {
+  for ( unsigned int i = 1; i <= runs; ++i ) {
+      ztree<N> a;
+      ztree<N> b;
+      if ( ( a.to_ulong() == b.to_ulong() ) && !(a == b) ) {
+          cout << a << endl << b << endl << a.to_ulong() << b.to_ulong() << endl;
+          exit( 1 );
+        }
+    }
+
+}
+
+
+size_t distance ( const ztree<N>& a, const ztree<N>& b, size_t& visited ) {
+  visited = 0;
+
+  if ( a == b ) return 0;
   unordered_set<unsigned long> queued( 100 );
-  vector<ztree<N> > q;
+  deque<ztree<N> > q;
   q.push_back( a );
   queued.insert( a.to_ulong() );
-  size_t qindex = 0;
-                        // I am in a interval, all nodes at distance d, and generating all ones at distance d+1
-  size_t left = 0;      // if qindex surpass this limit I generated all nodes at distance d+1
-  size_t right = 1;     // limit for the nodes at distance d+1
-  size_t d = 0;         // distance from a in the current interval
 
-  while( qindex < q.size() ) {
+  // During BFS I scan sequentially all nodes at distance d, exploring the next level d+1.
+  // I set two extremes for two sets:
+  //   [0,left)       contains the nodes at distance d, d-1, d-2.. 0 from the source node a
+  //   [left,right)   contains the nodes at distance d+1 from a
+  // When [0,left) = [0,0) means that I have exhausted all the nodes at distance d, d-1..
+  // that means I generated all the nodes on the next level, so I can increase the distance
+  // from the source.
+  int left = 1;      // Initially I have node a at distance 0 from a, so [0,left) must contain only node 0
+  int right = 1;     // The right limit is the left limit: I have no known node at distance 1
+  size_t d = 0;      // distance from a in the current level
+  bool found = false;
+
+  while( q.size() != 0 ) {
+    // select first node in the deque and generates its outcoming star
     for ( unsigned int i = 1; i <= N; ++i ) {
-      ztree<N> newone = q[qindex];
+      ztree<N> newone = q.front();
       newone ^ i;
 
-      if ( newone == q[qindex] )
-        continue;
-
-      // se l'ho gia' visitato, passo oltre
+      // if I already queued (or visited) this node I simply skip it
       if ( queued.count( newone.to_ulong() ) > 0 )
         continue;
 
-      // se l'ho trovato, esco
+      // if I found it, exit from the loops
       if ( newone == b ) {
-        visited = queued.size();
-        load = queued.load_factor();
-        buckets = queued.bucket_count();
-        return d + 1;
+        found = true;
+        break;
       }
 
+      // otherwise put the new node in the deque and to the hashtable
       q.push_back( newone );
       queued.insert( newone.to_ulong() );
       right++;
     }
 
-    qindex++;
+    if ( found ) break;
 
-    if ( qindex > left ) {
-      left = right - 1;
-      d++;
+    // effectively pop the first element, after visiting him
+    q.pop_front();
+    --right;
+    // start processing elements at the next level?
+    if ( --left == 0 ) {
+      left = right;
+      ++d;
     }
   }
 
+  if (!found) {
+      cerr << "OMG.\n";
+      cout << queued.count( a.to_ulong() ) << " " << queued.count( b.to_ulong() ) << endl;
+      exit( 1 );
+  }
+
   visited = queued.size();
-  load = queued.load_factor();
-  buckets = queued.bucket_count();
-  return d;
+  return d + 1;
 }
 
 
@@ -72,8 +100,7 @@ int main ( int argv, char** argc ) {
   if ( argv < 2 || atoi( argc[1] ) <= 0 ) return 1;
   int runs = atoi( argc[1] );
 
-
-  cout << "first\tsecond\topt\tdist\tcent\tvisited\tload\tbuckets\n";
+  cout << "first\tsecond\topt\tdist\tcent\tvisited\n";
   // genera runs coppie di alberi casuali con n nodi
   for (int i = 0; i < runs; ++i ) {
     ztree<N> a;
@@ -81,10 +108,9 @@ int main ( int argv, char** argc ) {
     ptree<int> aa( a ), aaa( a );
     ptree<int> bb( b ), bbb( b );
 
-    float load;
-    size_t visited, buckets;
+    size_t visited;
     int toptimal, tdistance, toldistance;
-    toptimal = distance(a, b, visited, load, buckets);
+    toptimal = distance(a, b, visited );
     tdistance = aa.distance( bb );
     toldistance = aaa.oldistance( bbb );
 
@@ -92,8 +118,8 @@ int main ( int argv, char** argc ) {
     assert( aaa == bbb );
     assert( toptimal <= tdistance );
 
-    cout << a << "\t" << b << "\t" << toptimal << "\t" << tdistance << "\t" << toldistance << "\t" << visited << "\t";
-    cout << load << "\t" << buckets << endl;
+    cout << a << "\t" << b << "\t" << toptimal << "\t" << tdistance << "\t" << toldistance << "\t" << visited << endl;
+    assert( toptimal == distance(b,a,visited) );
   }
 
 	return 0;
